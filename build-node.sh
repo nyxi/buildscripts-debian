92158958b24b9ef63135d3d9263e0bd34ae1aba1
Emil Flink <emil.flink@gmail.com>
2013-10-29 13:29:47 +0100

    Moar stuff

--- build-node.sh
+++ build-node.sh
@@ -12,7 +12,25 @@ tar xvf node-latest.tar.gz
 rm node-latest.tar.gz
 cd node-v*
 ./configure
-checkinstall -y --pkgversion "$(pwd | tail -c 8)" --install=no
+#####################
+# Control files
+#####################
+mkdir -p tmp/DEBIAN
+echo "Package: node" >> tmp/DEBIAN/control
+echo "Maintainer: $MAINTAINER" >> tmp/DEBIAN/control
+echo "Architecture: amd64" >> tmp/DEBIAN/control
+echo "Version: $(pwd | tail -c 8)" >> tmp/DEBIAN/control
+echo "Provides: node" >> tmp/DEBIAN/control
+echo "Priority: extra" >> tmp/DEBIAN/control
+echo "Section: main" >> tmp/DEBIAN/control
+echo "Filename: pool/main/n/node/node_$(pwd | tail -c 8)_amd64.deb" >> tmp/DEBIAN/control
+echo "Description: nodejs" >> tmp/DEBIAN/control
+chmod -R a-s tmp/DEBIAN
+make DESTDIR="$(pwd)"/tmp install
+echo "#!/bin/sh" >> tmp/DEBIAN/postinst
+echo "ldconfig" >> tmp/DEBIAN/postinst
+chmod 755 tmp/DEBIAN/postinst
+dpkg-deb --build tmp "node_$(pwd | tail -c 8)_amd64.deb"
 rm "$DEBDIR/node*deb" 2> /dev/null
-mv node*deb "$DEBDIR/"
+mv *deb "$DEBDIR/"
 echo "$(date) - nodejs $(pwd | tail -c 8) build ready" >> "$LOGFILE"
