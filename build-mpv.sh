2e818adf1ccb058a4241cf1b577b55311a0528b3
Emil Flink <emil.flink@gmail.com>
2014-01-19 17:46:36 +0100

    New way to build MPV

--- build-mpv.sh
+++ build-mpv.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#This script assumes you have dependencies set up
+#and the mpv-build git repo in $BUILDDIR/mpv-build
 
 BUILDDIR="/root/build"
 DEBDIR="/root/latestbuilds"
@@ -6,44 +8,31 @@ MAINTAINER="root@stor.nyxi.eu"
 LOGFILE="/root/logbuilds"
 
 #####################
-# Cleanup
+# Control files
 #####################
-cd "$BUILDDIR"
-rm mpv*deb
-cd "$BUILDDIR/mpv-build"
-#####################
-
-#####################
-# Update source, build
-# package with dependencies
-# and install it
-#####################
-./update
-rm *deb
-mk-build-deps
-dpkg -i *deb
-apt-get update && apt-get -f install -y
-#####################
-
-#####################
-# Overwite/create
-# debian/changelog
-#####################
-echo "mpv (1.0git-$(date +%Y%m%d)) stable; urgency=low" > debian/changelog
-echo "" >> debian/changelog
-echo "  * Initial package." >> debian/changelog
-echo "" >> debian/changelog
-echo " -- Wessel Dankers <wsl@fruit.je>  Sun, 06 Jan 2013 13:44:11 +0100" >> debian/changelog
+cd $BUILDDIR/mpv-build
+mkdir -p tmp/DEBIAN 2> /dev/null
+rm -f tmp/DEBIAN/control 2> /dev/null
+echo "Package: mpv" >> tmp/DEBIAN/control
+echo "Maintainer: $MAINTAINER" >> tmp/DEBIAN/control
+echo "Architecture: amd64" >> tmp/DEBIAN/control
+echo "Version: 1.0git-$(date +%Y%m%d)" >> tmp/DEBIAN/control
+echo "Provides: mpv" >> tmp/DEBIAN/control
+echo "Priority: optional" >> tmp/DEBIAN/control
+echo "Section: main" >> tmp/DEBIAN/control
+echo "Filename: pool/main/m/mpv/mpv_1.0git-$(date +%Y%m%d)_amd64.deb" >> tmp/DEBIAN/control
+echo "Description: mpv movie player" >> tmp/DEBIAN/control
+chmod -R a-s tmp/DEBIAN
 #####################
 
 #####################
 # Build it!
 #####################
-debuild -uc -us -b -j2
-cd /root/build
-rm *changes
-rm $BUILDDIR/*build 2> /dev/null
+./rebuild -j4
+mkdir -p tmp/usr/bin
+cp -f mpv/build/mpv tmp/usr/bin/
+dpkg-deb --build tmp "mpv_1.0git-$(date +%Y%m%d)_amd64.deb"
 rm $DEBDIR/mpv*deb 2> /dev/null
-mv mpv*deb "$DEBDIR/"
-echo "$(date) - MPV build ready" >> "$LOGFILE"
+mv *deb "$DEBDIR/"
+echo "$(date) - mpv build ready" >> "$LOGFILE"
 #####################
